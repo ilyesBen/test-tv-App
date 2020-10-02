02f6106b72cc62eb99a610d01d336dbae69fcba7
Ilyes Benmansour <ilyesbenmansour@JazzBook-Pro.local>
2020-10-02 23:13:19 +0100

    more navigation setup

--- src/index.js
+++ src/index.js
@@ -1,48 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
-import React, {useEffect} from 'react';
-import ReactNative, {StyleSheet, Platform, View, FlatList} from 'react-native';
-
-import {useVideos} from '@modules/videos';
-import {Video, Header} from '@components';
-
-const StatusBar = Platform.isTV ? View : ReactNative.StatusBar;
-
-const styles = StyleSheet.create({
-  listContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#696969',
-  },
-});
+import {AppNavigator} from '@navigator';
 
 const App: () => React$Node = () => {
-  const videos = useVideos();
-
-  const renderItem = ({item}) => {
-    return <Video video={item} onPress={() => alert('Hello guys')} />;
-  };
-
   return (
-    <>
-      <Header />
-      <View style={styles.listContainer}>
-        <FlatList
-          data={videos}
-          renderItem={renderItem}
-          keyExtractor={(item, index) => `${item.id}-${index}`}
-          numColumns={3}
-          scrollEnabled
-        />
-      </View>
-    </>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 };
 
